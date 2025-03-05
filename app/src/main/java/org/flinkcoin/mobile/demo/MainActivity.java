9f3a9d4866ffd2f3a6cecbb5a711ed69fae4ba13
netderecho <netderecho@flinkrnd.com>
2025-03-05 20:23:05 +0100

    added add NFT fragment

--- app/src/main/java/org/flinkcoin/mobile/demo/MainActivity.java
+++ app/src/main/java/org/flinkcoin/mobile/demo/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_payment_requests, R.id.nav_contacts, R.id.nav_transactions)
+                R.id.nav_home, R.id.nav_payment_requests, R.id.nav_nfts, R.id.nav_contacts, R.id.nav_transactions)
                 .setOpenableLayout(drawer)
                 .build();
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
