a5b1a12e719703974fd7494582349cf6c7a89dce
netderecho <netderecho@flinkrnd.com>
2025-03-09 23:53:59 +0100

    get PDQ hash from selected image

--- app/src/main/java/org/flinkcoin/mobile/demo/ui/nft/NftCreateHashFragment.java
+++ app/src/main/java/org/flinkcoin/mobile/demo/ui/nft/NftCreateHashFragment.java
@@ -1,5 +1,7 @@
 package org.flinkcoin.mobile.demo.ui.nft;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +13,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import org.flinkcoin.mobile.demo.databinding.FragmentNftCreateHashBinding;
 
+import java.io.FileNotFoundException;
+
 import pdqhashing.hasher.PDQHasher;
+import pdqhashing.types.HashAndQuality;
+import pdqhashing.utils.MatrixUtil;
 
 public class NftCreateHashFragment extends Fragment {
 
@@ -28,8 +34,24 @@ public class NftCreateHashFragment extends Fragment {
         binding.buttonConfirm.setOnClickListener(v -> {
 
             PDQHasher hasher = new PDQHasher();
-            //TODO: rewrite hasher
+            Bitmap bitmap = null;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(nftViewModel.getSelectedImage()));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
 
+            int numRows = bitmap.getHeight();
+            int numCols = bitmap.getWidth();
+            float[] buffer1 = MatrixUtil.allocateMatrixAsRowMajorArray(numRows, numCols);
+            float[] buffer2 = MatrixUtil.allocateMatrixAsRowMajorArray(numRows, numCols);
+            float[][] buffer64x64 = MatrixUtil.allocateMatrix(64, 64);
+            float[][] buffer16x64 = MatrixUtil.allocateMatrix(16, 64);
+            float[][] buffer16x16 = MatrixUtil.allocateMatrix(16, 16);
+
+            HashAndQuality hashAndQuality = hasher.fromBitmap(bitmap, buffer1, buffer2, buffer64x64, buffer16x64, buffer16x16);
+
+            //TODO: save, publish new block
 
         });
 
