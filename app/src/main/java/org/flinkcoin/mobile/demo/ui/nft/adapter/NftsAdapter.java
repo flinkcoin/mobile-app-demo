ea8b8feea250a0d3546b12845304036eb228902c
netderecho <netderecho@flinkrnd.com>
2025-03-12 22:39:16 +0100

    add nft adapter

---
+++ app/src/main/java/org/flinkcoin/mobile/demo/ui/nft/adapter/NftsAdapter.java
@@ -0,0 +1,61 @@
+package org.flinkcoin.mobile.demo.ui.nft.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.flinkcoin.mobile.demo.R;
+import org.flinkcoin.mobile.demo.data.model.NftData;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class NftsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final Consumer<NftData> onClickConsumer;
+
+    private List<NftDataItem> itemsDataSet;
+
+    public NftsAdapter(Consumer<NftData> onClickConsumer) {
+        this.onClickConsumer = onClickConsumer;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.nft_row_item, viewGroup, false);
+        return new NftViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int position) {
+        NftDataItem nftDataItem = itemsDataSet.get(position);
+
+        NftData nftData = nftDataItem.getNftData();
+        NftViewHolder nftViewHolder = (NftViewHolder) viewHolder;
+
+        nftViewHolder.getTransactionType().setImageResource(R.drawable.ic_receive_transaction);
+        nftViewHolder.getNftCode().setText(nftData.getMaskedNftCode());
+        nftViewHolder.getAccountCode().setText(nftData.getAccountCode());
+        nftViewHolder.getNftPreview().setImageResource(R.drawable.ic_question_mark);
+
+        if (onClickConsumer != null) {
+            viewHolder.itemView.setOnClickListener(v -> onClickConsumer.accept(nftData));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemsDataSet == null ? 0 : itemsDataSet.size();
+    }
+
+    public void setItems(List<NftDataItem> items) {
+        this.itemsDataSet = items;
+        notifyDataSetChanged();
+    }
+}
