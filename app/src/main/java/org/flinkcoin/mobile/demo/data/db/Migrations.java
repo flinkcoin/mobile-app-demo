901faea2c5a7cb4af39ad21dcda5f1b1afbc0443
netderecho <netderecho@flinkrnd.com>
2025-03-13 14:51:40 +0100

    add account_code to account table, db migration

--- app/src/main/java/org/flinkcoin/mobile/demo/data/db/Migrations.java
+++ app/src/main/java/org/flinkcoin/mobile/demo/data/db/Migrations.java
@@ -1,18 +1,17 @@
 package org.flinkcoin.mobile.demo.data.db;
 
 import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class Migrations {
 
-    /*private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("");
+            database.execSQL("ALTER TABLE account ADD COLUMN account_code TEXT DEFAULT ''");
         }
-    };*/
+    };
 
-
-
-    public static final Migration[] ALL_MIGRATIONS = new Migration[]{};
+    public static final Migration[] ALL_MIGRATIONS = new Migration[]{MIGRATION_1_2};
 
 }
